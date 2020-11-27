de571aad78706eac7fec9301303e4d6c7ccac304
Christopher Dang <christhd@uci.edu>
2020-11-27 10:30:14 -0800

    Cut down sound files. Started mapping the floors for footstep sounds

---
+++ Assets/Scripts/MapScripts/DoorCloser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCloser: MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Door;
+    [SerializeField]
+    private GameObject openDoorTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            closeDoor();
+        }
+    }
+
+    private void closeDoor() {
+        Door.GetComponent<Transform>().Rotate(0, 0, -90, Space.Self);
+        openDoorTrigger.GetComponent<BoxCollider>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/MapScripts/DoorOpener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Door;
+    [SerializeField]
+    private GameObject openDoorTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            openDoor();
+        }
+    }
+
+    private void openDoor()
+    {
+        Door.GetComponent<Transform>().Rotate(0, 0, 90, Space.Self);
+        openDoorTrigger.GetComponent<BoxCollider>().enabled = false;
+    }
+}
