f75d946e19642557e2feeb43d017e67992e1c9b1
Christopher Dang <christhd@uci.edu>
2020-11-26 20:59:22 -0800

    Added Door Trigger Script to close door on trigger -Should be edited to prompt the user first then close the door
    Added box colliders to Comps and doors

---
+++ Assets/Scripts/MultiplayerScripts/EnemyAIPathing.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAIPathing : MonoBehaviour
+{
+    //Used to find waypoints
+    public Transform[] wayPoints;
+    private int wayPointIndex;
+
+    //ini
+    NavMeshAgent agent;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    private bool walkPointSet;
+    private bool goingForward; //used to scout back and forth
+
+
+    //Chasing
+    private Transform target = null;
+    public float chaseRange;
+    private bool isChasing;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        wayPointIndex = 0;
+        walkPointSet = false;
+        goingForward = true;
+        isChasing = false;
+        transform.LookAt(wayPoints[wayPointIndex].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null && isChasing == true)
+        {
+            Chasing();
+        }
+        else
+        {
+            Patrolling();
+        }
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToNextPoint = transform.position - walkPoint;
+        //Walkpoint Reached
+        if (distanceToNextPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+            NextIndex();
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        walkPoint = wayPoints[wayPointIndex].position;
+        walkPointSet = true;
+    }
+
+    private void NextIndex()
+    {
+        if (goingForward)
+        {
+            wayPointIndex++;
+            if (wayPointIndex >= wayPoints.Length)
+            {
+                wayPointIndex--;
+                goingForward = false;
+            }
+        }
+
+        if (!goingForward)
+        {
+            wayPointIndex--;
+            if (wayPointIndex < 0)
+            {
+                wayPointIndex++;
+                goingForward = true;
+            }
+        }
+
+        transform.LookAt(wayPoints[wayPointIndex].position);
+    }
+
+    private void Chasing()
+    {
+
+        float distanceFromTarget = Vector3.Distance(transform.position, target.transform.position);
+
+        if (distanceFromTarget > chaseRange)
+        {
+            target = null;
+            isChasing = false;
+        }
+        else
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            target = other.transform;
+            isChasing = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        /*if (other.tag == "Player")
+        {
+            target = null;
+        }*/
+    }
+}
