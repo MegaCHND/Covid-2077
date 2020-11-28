29b405203a26c94271de0f73a473f43549782757
Christopher Dang <christhd@uci.edu>
2020-11-28 13:30:04 -0800

    Added a footsteps script that knows what floor type is under the player and plays footsteps.
    Added tags for later use. Map needs to have tags added
    
    Bugs
    -Footsteps play late. Need to clip the beginning of sounds down more
    -Server/client connect is broke. Player on the server doesnt update anymore

--- Assets/Scripts/MapScripts/PlayFootstepSound.cs
+++ Assets/Scripts/MapScripts/PlayFootstepSound.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayFootstepSound : MonoBehaviour
 {
+    public AudioClip[] FootSoundsArray;
+    private AudioSource audioSource;
+    private Transform Tf;
+    private Vector3 temp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Tf = GetComponent<Transform>();
+        audioSource = GetComponent<AudioSource>();
+        temp = Tf.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (temp != Tf.position) {
+            playSound();
+        }
+    }
+
+    /*Tags for footstep sounds are...
+     * Vent
+     * carpet
+     * tile
+     * hardwood
+     * pseudo-cement*/
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Tiggered!");
+        if (other.CompareTag("Vent"))
+        {
+            audioSource.clip = FootSoundsArray[4];
+        }
+        else if (other.CompareTag("tile"))
+        {
+            audioSource.clip = FootSoundsArray[3];
+        }
+        else if (other.CompareTag("hardwood "))
+        {
+            audioSource.clip = FootSoundsArray[0];
+        }
+        else if (other.CompareTag("pseudo-cement "))
+        {
+            audioSource.clip = FootSoundsArray[2];
+        }
+        else if (other.CompareTag("carpet "))
+        {
+            audioSource.clip = FootSoundsArray[1];
+        }
+    }
+
+    void playSound() {
+        audioSource.volume = Random.Range(.8f, 1);
+        audioSource.pitch = Random.Range(.8f, 1.1f);
+        audioSource.Play();
+        temp = Tf.position;
+        print("playing sound" + audioSource.clip);
     }
 }
