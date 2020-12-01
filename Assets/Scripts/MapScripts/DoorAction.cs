30ed948b4d92e0f83c944a0edaa3cd784f7dad7b
cagomez6 <64633124+cagomez6@users.noreply.github.com>
2020-11-30 16:00:51 -0800

    readded door scripts
    Doors keep track of their status and open/close when their function is called by any other object/trigger

---
+++ Assets/Scripts/MapScripts/DoorAction.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAction : MonoBehaviour
+{
+    public bool isOpen;
+    public float rotationDegree;
+    public AudioClip ACOpen;
+    public AudioClip ACClose;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void openDoor()
+    {
+        if (!isOpen)
+        {
+            gameObject.transform.Rotate(0, 0, rotationDegree, Space.Self);
+            isOpen = true;
+            playOpen();
+        }
+    }
+
+    public void closeDoor()
+    {
+        if (isOpen)
+        {
+            gameObject.transform.Rotate(0, 0, -rotationDegree, Space.Self);
+            isOpen = false;
+            playClose();
+        }
+    }
+
+    private void playOpen()
+    {
+        audioSource.Stop();
+        audioSource.clip = ACOpen;
+        audioSource.Play();
+    }
+
+    private void playClose()
+    {
+        audioSource.Stop();
+        audioSource.clip = ACClose;
+        audioSource.Play();
+    }
+}
