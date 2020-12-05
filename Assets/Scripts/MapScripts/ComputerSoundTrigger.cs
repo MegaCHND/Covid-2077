a1321b2df12701731b2beb958bbd24a7d065ff36
TigAurora <tigaurora@gmail.com>
2020-12-04 18:31:36 -0800

    Add Beep Script and Adjust audiosource
    Add Beep Script in sample scene and Adjust audiosource range in the server scene

---
+++ Assets/Scripts/MapScripts/ComputerSoundTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerSoundTrigger : MonoBehaviour
+{
+    public GameObject computer;
+    private int playersAmount = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            computer.GetComponent<AudioSource>().Play();
+            ++playersAmount;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            --playersAmount;
+            if (playersAmount <= 0)
+            {
+                computer.GetComponent<AudioSource>().Stop();
+                playersAmount = 0;
+            }
+        }
+    }
+}
